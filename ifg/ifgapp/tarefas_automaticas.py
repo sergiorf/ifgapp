ff858c794ee091226b2fdec5e6a5d14423155ba2
sergio rodriguez <sergio.rodriguez.freire@gmail.com>
2016-01-17 15:34:43 -0200

    dev

--- ifg/ifgapp/tarefas_automaticas.py
+++ ifg/ifgapp/tarefas_automaticas.py
@@ -13,12 +13,14 @@ class MetaTarefa():
 PRIMEIRA_ANUIDADE = u'1a Anuidade'
 ANUIDADE = u'Anuidade'
 DESARQUIVAMENTO = u'Desarquivamento: anuidade não paga'
-
-
+PEDIDO_EXAME = u'Pedido de exame'
+DESARQUIVAMENTO_EXAME = u'Desarquivamento: exame não solicitado'
 Tarefas = {
     PRIMEIRA_ANUIDADE: MetaTarefa(nome=PRIMEIRA_ANUIDADE, tipo_atividade_pk=2, atividade_pk=7, shift=relativedelta(months=+36)),
     ANUIDADE: MetaTarefa(nome=ANUIDADE, tipo_atividade_pk=2, atividade_pk=7, shift=relativedelta(months=+3)),
-    DESARQUIVAMENTO: MetaTarefa(nome=DESARQUIVAMENTO, tipo_atividade_pk=2, atividade_pk=7, shift=relativedelta(months=+3))
+    DESARQUIVAMENTO: MetaTarefa(nome=DESARQUIVAMENTO, tipo_atividade_pk=2, atividade_pk=7, shift=relativedelta(months=+3)),
+    PEDIDO_EXAME: MetaTarefa(nome=PEDIDO_EXAME, tipo_atividade_pk=1, atividade_pk=4, shift=relativedelta(months=+36)),
+    DESARQUIVAMENTO_EXAME: MetaTarefa(nome=DESARQUIVAMENTO_EXAME, tipo_atividade_pk=1, atividade_pk=4, shift=relativedelta(days=+60)),
 }
 
 
@@ -28,26 +30,45 @@ def cria_tarefas(tecnologia):
     else:
         pass
 
+
 def cria_tarefas_marca(tecnologia):
     if tecnologia.pedido is not None:
         anuidade = get_last_anuidade(tecnologia)
         if anuidade is None:
-            cria_tarefa(tecnologia, tecnologia.pedido, 1, Tarefas[PRIMEIRA_ANUIDADE])
+            cria_tarefa2(tecnologia, tecnologia.pedido, 1, Tarefas[PRIMEIRA_ANUIDADE])
         elif anuidade.status == Tarefa.FINALIZADA:
             start_dt = date(date.today().year+1, 1, 1)
-            cria_tarefa(tecnologia, start_dt, anuidade.anuidade_nr+1, Tarefas[ANUIDADE])
+            cria_tarefa2(tecnologia, start_dt, anuidade.anuidade_nr+1, Tarefas[ANUIDADE])
         elif anuidade.status == Tarefa.NAO_REALIZADA:
-            cria_tarefa(tecnologia, anuidade.realizacao_final, -1, Tarefas[DESARQUIVAMENTO])
+            cria_tarefa(tecnologia, anuidade.realizacao_final, Tarefas[DESARQUIVAMENTO])
+        tf = get_tarefa(tecnologia, PEDIDO_EXAME)
+        if tf is None:
+            cria_tarefa(tecnologia, tecnologia.pedido, Tarefas[PEDIDO_EXAME])
+        elif tf.status == Tarefa.NAO_REALIZADA:
+            cria_tarefa(tecnologia, tf.realizacao_final, Tarefas[DESARQUIVAMENTO_EXAME])
 
 
 def get_last_anuidade(tecnologia):
-    q = Tarefa.objects.filter(tecnologia=tecnologia).order_by('realizacao_inicio')
+    q = Tarefa.objects.filter(tecnologia=tecnologia).filter(nome__contains=ANUIDADE).order_by('realizacao_inicio')
     if len(q) > 0:
         return q[0]
     else:
         return None
 
-def cria_tarefa(tecnologia, start_dt, anuidade_nr, meta_tarefa):
+
+def get_tarefa(tecnologia, nome):
+    q = Tarefa.objects.filter(tecnologia=tecnologia).filter(nome=nome)
+    if len(q) > 0:
+        return q[0]
+    else:
+        return None
+
+
+def cria_tarefa(tecnologia, start_dt, meta_tarefa):
+    return cria_tarefa2(tecnologia, start_dt, 0, meta_tarefa)
+
+
+def cria_tarefa2(tecnologia, start_dt, anuidade_nr, meta_tarefa):
     tf = Tarefa()
     tf.nome = str(anuidade_nr) + u'a ' + meta_tarefa.nome
     tf.tecnologia = tecnologia
